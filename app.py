bbc2a033129b7f4a7622482b79ae3de6ed531d20
Nikhil Joshi <nick0702cloud@gmail.com>
2025-07-29 23:38:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ import streamlit as st
 import pickle
 import string
 
+import os
 import nltk
 
-nltk.download('punkt')
+nltk_data_path = os.path.join(os.path.expanduser("~"), "nltk_data")
+nltk.data.path.append(nltk_data_path)
+
+try:
+    nltk.data.find("tokenizers/punkt")
+except LookupError:
+    nltk.download("punkt", download_dir=nltk_data_path)
+
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
